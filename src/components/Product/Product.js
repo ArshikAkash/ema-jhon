a3b75e4fbe89092c9d7d30856a4dba73e7999b7b
ArshikAkash <arshikakash@gmail.com>
2021-03-01 21:12:00 +0600

    practice1 done

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 const Product = (props) => {
 	console.log(props);
+	const { img, name, seller, price, stock } = props.product;
 	return (
-		<div>
-			<h2>This is product</h2>
-			<h3>{props.product.name}</h3>
+		<div className='product'>
+			<div>
+				<img src={img} alt='' />
+			</div>
+			<div className='product-name'>
+				<h3>{name}</h3>
+				<br />
+				<p>
+					<small>by:{seller} </small>
+				</p>
+				<b>${price}</b>
+				<p>
+					<small>Only {stock} left, Order soon.</small>
+				</p>
+				<button
+					className='main-button'
+					onClick={() => props.handleAddProduct(props.product)}
+				>
+					<FontAwesomeIcon icon={faShoppingCart} /> Add To Cart
+				</button>
+			</div>
 		</div>
 	);
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,22 +1,28 @@
 import React, { useState } from 'react';
 import fakeData from '../../fakeData';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 const Shop = () => {
 	const first10 = fakeData.slice(0, 10);
 	const [products] = useState(first10);
+	const [cart, setCart] = useState([]);
+	const handleAddProduct = (product) => {
+		const newCart = [...cart, product];
+		setCart(newCart);
+	};
 
 	return (
 		<div className='shop-container'>
 			<div className='product-container'>
-				<h3>{products.length}</h3>
-
 				{products.map((product) => (
-					<Product product={product}>{product.name}</Product>
+					<Product handleAddProduct={handleAddProduct} product={product}>
+						{product.name}
+					</Product>
 				))}
 			</div>
 			<div className='cart-container'>
-				<h2>this is cart</h2>
+				<Cart cart={cart}></Cart>
 			</div>
 		</div>
 	);
